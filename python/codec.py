33ed2bc2b20c472d66dbd7964b5855f4ba6c607c
Magnus Lundmark <magnus@skysense.io>
2022-11-15 16:05:43 +0100

    change to bytearray, compute payload crc as well

--- python/codec.py
+++ python/codec.py
@@ -5,10 +5,6 @@ from random import sample
 from crcmod import mkCrcFun
 from struct import pack
 
-# Poly, Initial = 0x00, No reflect, FinalXOR = 0x00
-# 1 added from the left to poly
-compute_crc = mkCrcFun(0x1864cfb, 0x00, False, 0x00) # returns integer
-
 libdt = ct.cdll.LoadLibrary("libdt.so")
 libdt.dt_turbo_fwd.restype  = ct.c_uint32
 libdt.dt_turbo_fwd.argtypes = [ct.POINTER(ct.c_uint8), ct.POINTER(ct.c_uint8)]
@@ -24,6 +20,60 @@ droneid["zc_root_symbol_6"] = 147
 #droneid["cp_seq"]           = [1, 0, 0, 0, 0, 0, 0, 0, 1] # 1 is long
 droneid["cp_seq"]           = [0, 0, 0, 0, 0, 0, 0, 1] # 1 is long
 droneid["models"]           = { 16: "Mavic Pro", 41: "Mavic 2", 61: "DJI FPV", 63: "Mini 2", 68: "Mavic 3" }
+
+# Poly, Initial = 0x00, No reflect, FinalXOR = 0x00
+# 1 added from the left to poly
+frame_crc_fct = mkCrcFun(0x1864cfb, 0x00, False, 0x00) # returns integer
+
+def frame_crc(data):
+    res = frame_crc_fct(data)
+    crc = bytearray(3)
+    crc[0] = ((res >>  0) & 0xff);
+    crc[1] = ((res >>  8) & 0xff);
+    crc[2] = ((res >> 16) & 0xff);
+    return crc
+
+def payload_crc(data):
+    init = 0x3692
+    table = (0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
+            0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7,
+            0x1081, 0x0108, 0x3393, 0x221a, 0x56a5, 0x472c, 0x75b7, 0x643e,
+            0x9cc9, 0x8d40, 0xbfdb, 0xae52, 0xdaed, 0xcb64, 0xf9ff, 0xe876,
+            0x2102, 0x308b, 0x0210, 0x1399, 0x6726, 0x76af, 0x4434, 0x55bd,
+            0xad4a, 0xbcc3, 0x8e58, 0x9fd1, 0xeb6e, 0xfae7, 0xc87c, 0xd9f5,
+            0x3183, 0x200a, 0x1291, 0x0318, 0x77a7, 0x662e, 0x54b5, 0x453c,
+            0xbdcb, 0xac42, 0x9ed9, 0x8f50, 0xfbef, 0xea66, 0xd8fd, 0xc974,
+            0x4204, 0x538d, 0x6116, 0x709f, 0x0420, 0x15a9, 0x2732, 0x36bb,
+            0xce4c, 0xdfc5, 0xed5e, 0xfcd7, 0x8868, 0x99e1, 0xab7a, 0xbaf3,
+            0x5285, 0x430c, 0x7197, 0x601e, 0x14a1, 0x0528, 0x37b3, 0x263a,
+            0xdecd, 0xcf44, 0xfddf, 0xec56, 0x98e9, 0x8960, 0xbbfb, 0xaa72,
+            0x6306, 0x728f, 0x4014, 0x519d, 0x2522, 0x34ab, 0x0630, 0x17b9,
+            0xef4e, 0xfec7, 0xcc5c, 0xddd5, 0xa96a, 0xb8e3, 0x8a78, 0x9bf1,
+            0x7387, 0x620e, 0x5095, 0x411c, 0x35a3, 0x242a, 0x16b1, 0x0738,
+            0xffcf, 0xee46, 0xdcdd, 0xcd54, 0xb9eb, 0xa862, 0x9af9, 0x8b70,
+            0x8408, 0x9581, 0xa71a, 0xb693, 0xc22c, 0xd3a5, 0xe13e, 0xf0b7,
+            0x0840, 0x19c9, 0x2b52, 0x3adb, 0x4e64, 0x5fed, 0x6d76, 0x7cff,
+            0x9489, 0x8500, 0xb79b, 0xa612, 0xd2ad, 0xc324, 0xf1bf, 0xe036,
+            0x18c1, 0x0948, 0x3bd3, 0x2a5a, 0x5ee5, 0x4f6c, 0x7df7, 0x6c7e,
+            0xa50a, 0xb483, 0x8618, 0x9791, 0xe32e, 0xf2a7, 0xc03c, 0xd1b5,
+            0x2942, 0x38cb, 0x0a50, 0x1bd9, 0x6f66, 0x7eef, 0x4c74, 0x5dfd,
+            0xb58b, 0xa402, 0x9699, 0x8710, 0xf3af, 0xe226, 0xd0bd, 0xc134,
+            0x39c3, 0x284a, 0x1ad1, 0x0b58, 0x7fe7, 0x6e6e, 0x5cf5, 0x4d7c,
+            0xc60c, 0xd785, 0xe51e, 0xf497, 0x8028, 0x91a1, 0xa33a, 0xb2b3,
+            0x4a44, 0x5bcd, 0x6956, 0x78df, 0x0c60, 0x1de9, 0x2f72, 0x3efb,
+            0xd68d, 0xc704, 0xf59f, 0xe416, 0x90a9, 0x8120, 0xb3bb, 0xa232,
+            0x5ac5, 0x4b4c, 0x79d7, 0x685e, 0x1ce1, 0x0d68, 0x3ff3, 0x2e7a,
+            0xe70e, 0xf687, 0xc41c, 0xd595, 0xa12a, 0xb0a3, 0x8238, 0x93b1,
+            0x6b46, 0x7acf, 0x4854, 0x59dd, 0x2d62, 0x3ceb, 0x0e70, 0x1ff9,
+            0xf78f, 0xe606, 0xd49d, 0xc514, 0xb1ab, 0xa022, 0x92b9, 0x8330,
+            0x7bc7, 0x6a4e, 0x58d5, 0x495c, 0x3de3, 0x2c6a, 0x1ef1, 0x0f78)
+    crc = init
+    for i in range(len(data)):
+        crc = (crc >> 8) ^ table[(crc ^ data[i]) & 0x00ff]
+    res = bytearray(2)
+    res[0] = ((crc >>  0) & 0xff);
+    res[1] = ((crc >>  8) & 0xff);
+    return res
 
 def msg_to_bb(msg_dict=None, samp_rate=15.36e6):
     msg = encode(msg_dict)
@@ -148,38 +198,66 @@ def short_cp_size(samp_rate):
 def long_cp_size(samp_rate):
     return int(np.round(samp_rate / 192000.0))
 
-def dji_crc(data):
-    res = compute_crc(data)
-    crc = bytearray(3)
-    crc[0] = ((res >> 16) & 0xff);
-    crc[1] = ((res >>  8) & 0xff);
-    crc[2] = ((res >>  0) & 0xff);
-    return crc
+def droneid_fields():
+    k = ['packet_len']
+    k.append('packet_type')
+    k.append('version')
+    k.append('sequence_num')
+    k.append('state_info')
+    k.append('serial')
+    k.append('uav_lat')
+    k.append('uav_lon')
+    k.append('uav_height')
+    k.append('uav_alt')
+    k.append('uav_vel_n')
+    k.append('uav_vel_e')
+    k.append('uav_vel_u')
+    k.append('uav_yaw')
+    k.append('pilot_time')
+    k.append('pilot_lat')
+    k.append('pilot_lon')
+    k.append('home_lat')
+    k.append('home_lon')
+    k.append('product_type')
+    k.append('uuid_len')
+    k.append('uuid')
+    k.append('terminator')
+    k.append('scrap')
+    k.append('crc')
+    return k
 
 def frame(msg={}):
-    ############################################################################
-    # FIELD         START  LEN  ENCODING     COMMENT
-    ############################################################################
-    # packet_len     0      1    uint8        Typically 88
-    # packet_type    1      1    uint8        Typically 16
-    # version        2      1    uint8        Typically 2
-    # sequence_num   3      2    le_int16     Running counter
-    # state_info     5      2    uint8[2]     unknown meaning, [0, 0]
-    # serial         7     16    ASCII        Serial string
-    # uav_lon       23      4    le_int32     UAV longitude [-180, 180], scaled 
-    # uav_lat       27      4    le_int32     UAV latitude [-90, 90], scaled
-    # uav_height    31      2    le_int16     UAV height in m
-    # uav_alt       33      2    le_int16     UAV altitude in m
-    # uav_vel_n     35      2    le_int16     UAV velocity North
-    # uav_vel_e     37      2    le_int16     UAV velocity East
-    # uav_vel_u     39      2    le_int16     UAV velocity Up
-    # uav_yaw       41      2    le_int16     UAV yaw
-    # pilot_time    43      8    le_uint64    Pilot UNIX time in ms
-    # pilot_lat     51      4    le_int32     Pilot latitude [-90, 90], scaled 
-    # pilot_lon     55      4    le_int32     Pilot longitude [-180, 180], scaled 
-    # home_lon      59      4    le_int32     Home longitude [-180, 180], scaled 
-    # home_lat      63      4    le_int32     Home latitude [-90, 90], scaled
-    # product_type  64      1    uint8        DJI product type
+    #################################################################################
+    #    FIELD      START  LEN  ENCODING          COMMENT
+    #################################################################################
+    # packet_len     0      1    uint8           Typically 88
+    # packet_type    1      1    uint8           Typically 16
+    # version        2      1    uint8           Typically 2
+    # sequence_num   3      2    le_int16        Running counter
+    # state_info     5      2    uint8[2]        unknown meaning, [0, 0]
+    # serial         7     16    uint8[16]       Serial string
+    # uav_lon       23      4    le_int32        UAV longitude [-180, 180], scaled 
+    # uav_lat       27      4    le_int32        UAV latitude [-90, 90], scaled
+    # uav_height    31      2    le_int16        UAV height in m
+    # uav_alt       33      2    le_int16        UAV altitude in m
+    # uav_vel_n     35      2    le_int16        UAV velocity North
+    # uav_vel_e     37      2    le_int16        UAV velocity East
+    # uav_vel_u     39      2    le_int16        UAV velocity Up
+    # uav_yaw       41      2    le_int16        UAV yaw
+    # pilot_time    43      8    le_uint64       Pilot UNIX time in ms
+    # pilot_lat     51      4    le_int32        Pilot latitude [-90, 90], scaled 
+    # pilot_lon     55      4    le_int32        Pilot longitude [-180, 180], scaled 
+    # home_lon      59      4    le_int32        Home longitude [-180, 180], scaled 
+    # home_lat      63      4    le_int32        Home latitude [-90, 90], scaled
+    # product_type  67      1    uint8           DJI product type
+    # uuid_len      68      1    uint8           uuid length, max 19
+    # uuid          69     19    uint8[19]       uuid, 0x00 fill at end
+    # terminator    88      1    uint8           0x00
+    # payload_crc   90      2    uint8           payload crc
+    # scrap_bytes   91     82    uint8           Seems to be 'DONT CARE'
+    # frame_crc    173      3    uint8           frame crc
+    ###################   176    uint8           ####################################
+    #################################################################################
 
     scale = 10e6 * np.pi / 180.
     frame = bytearray(176)
@@ -209,14 +287,15 @@ def frame(msg={}):
     frame[59:59 + 4] = pack('<i', lon)    
     lat = round(scale * msg["home_lat"])
     frame[63:63 + 4] = pack('<i', lat)
-    frame[64] = msg["product_type"]
+    frame[67] = msg["product_type"]
     uuid = [ ord(c) for c in msg["uuid"][:19] ]
-
-    frame[65] = len(uuid)
-    frame[66:66 + 19] = uuid + [0,] * (19 - len(uuid))
+    frame[68] = len(uuid)
+    frame[69:69 + 19] = uuid + [0,] * (19 - len(uuid))
+    frame[88] = 0x00
+    frame[90:90 + 2] = payload_crc(frame[:89])
     # Scrap values could be set here...
     # frame[]
-    frame[173:173 + 3] = dji_crc(frame[:173])
+    frame[173:173 + 3] = frame_crc(frame[:173])
 
     return frame
 
@@ -224,23 +303,21 @@ def frame(msg={}):
 
 def encode(msg_dict=None):
     if msg_dict is None:
-        msg = np.array([ 88,  16,   2,   0,   0,   0,   0,  48,  49,  50,  51,  52,  53,\
+        msg = bytearray([ 88,  16,   2,   0,   0,   0,   0,  48,  49,  50,  51,  52,  53,\
             54,  55,  56,  57,  97,  98,  99, 100,   0,   0,   0,   0,   0,\
              0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,\
              0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,\
              0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,\
              0,   0,   0,  19,   0,   0,   0,   0,   0,   0,   0,   0,   0,\
-             0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  76, 201], dtype=np.uint8) #Some known good frame
+             0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  76, 201]) #Some known good frame
     else:
-        msg = np.ndarray(91, dtype=np.uint8)
-        msg.fill(0xff)   
+        msg = bytearray([0xff for x in range(91)])
 
-    scrap = np.ndarray(82, dtype=np.uint8)
-    scrap.fill(0x00)
+    scrap = bytearray(82)
 
-    msg = np.concatenate([msg, scrap])
-    crc = dji_crc(msg)
-    return np.concatenate([msg, crc]) # 176 CRC augmented payload bytes
+    msg += scrap
+    msg += frame_crc(msg)
+    return msg # 176 CRC augmented payload bytes
 
 def turbo_fwd(msg):
     if len(msg) != 176:
