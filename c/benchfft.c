fcb82be0e2cb4ab88e2250f2f909ebe4e5454156
Magnus Lundmark <magnus@skysense.io>
2022-12-27 16:14:08 +0100

    cleanup wisdom generation

--- c/benchfft.c
+++ c/benchfft.c
@@ -10,15 +10,15 @@ gcc -o benchfft -march=native -O3 -std=c99 benchfft.c -lm -lfftw3
 
 float _Complex 
 fun(float _Complex x,float _Complex y){
-	return 1.234f + 4.567*I;
+    return 1.234f + 4.567*I;
 }
 
 void
 save(char* fn, const float _Complex *data, const size_t num) {
-	FILE* fp;
-	fp = fopen(fn,"wb");
-	fwrite((void*)data, sizeof(float _Complex), num, fp);
-	fclose(fp);
+    FILE* fp;
+    fp = fopen(fn,"wb");
+    fwrite((void*)data, sizeof(float _Complex), num, fp);
+    fclose(fp);
 }
 
 float
@@ -33,55 +33,51 @@ randn() {
 
 void
 gen_data(_Complex float *data, size_t num) {
-	float f1 = 33.f;
-	float f2 = 63.f;
-	float a1 = 1.f;
-	float a2 = 2.f;
-	float n = 1.f;
-	for (int i = 0; i < num; ++i)
-	{
-		*data  = a1 * cexp(I * 2.f * M_PI * f1 * (float) i / (float) num);
-		*data += a2 * cexp(I * 2.f * M_PI * f2 * (float) i / (float) num);
-		*data += n * M_SQRT1_2 * (randn() + I * randn());
-		data++;
-	}
-
-
+    float f1 = 33.f;
+    float f2 = 67.f;
+    float a1 = 1.234f;
+    float a2 = 2.345f;
+    float n = 1.f;
+    for (int i = 0; i < num; ++i) {
+        *data  = a1 * cexp(I * 2.f * M_PI * f1 * (float) i / (float) num);
+        *data += a2 * cexp(I * 2.f * M_PI * f2 * (float) i / (float) num);
+        *data += n * M_SQRT1_2 * (randn() + I * randn());
+        data++;
+    }
 }
 
 int
 main(int argc, char** argv) {
-	const size_t N = 1024*2;
-	float _Complex *in, *out;
-	in = (fftwf_complex*) fftwf_malloc( N * sizeof(fftwf_complex));
-	out = (fftwf_complex*) fftwf_malloc( N * sizeof(fftwf_complex));
-	fftwf_plan p;
-	
-	gen_data(in, N);
-	save("in.fc32", in, N);
-	printf("%s\n", fftwf_version);
+    const size_t N = 1024*2;
+    float _Complex *in, *out;
+    in = (fftwf_complex*) fftwf_malloc( N * sizeof(fftwf_complex));
+    out = (fftwf_complex*) fftwf_malloc( N * sizeof(fftwf_complex));
+    fftwf_plan p;
+    
+    gen_data(in, N);
+    save("in.fc32", in, N);
+    printf("%s\n", fftwf_version);
 
-	if (fftwf_import_wisdom_from_filename("fftw3f.wisdom")) {
-		printf("%s\n", "Imported wisdom");		
-	}
+    if (fftwf_import_wisdom_from_filename("fftw3f.wisdom")) {
+        printf("%s\n", "Imported wisdom");      
+    }
 
-	p = fftwf_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_WISDOM_ONLY);
-	if (!p) {
-		printf("%s\n", "There was no wisdom");
-		printf("%s\n", "Generating wisdom...");		
-		p = fftwf_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_PATIENT);
-		fftwf_export_wisdom_to_filename("fftw3f.wisdom");
-	} else {
-		printf("%s\n", "Generated plan from wisdom");
-	}
+    p = fftwf_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_WISDOM_ONLY);
+    if (!p) {
+        printf("%s\n", "There was no wisdom, generating...");
+        p = fftwf_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_PATIENT);
+        fftwf_export_wisdom_to_filename("fftw3f.wisdom");
+    } else {
+        printf("%s\n", "Generated plan from wisdom");
+    }
 
-	fftwf_execute(p);
-	printf("%s\n", "Executed FFT");
-	fftwf_destroy_plan(p);
+    fftwf_execute(p);
+    printf("%s\n", "Executed FFT");
+    fftwf_destroy_plan(p);
 
-	save("out.fc32", out, N);
+    save("out.fc32", out, N);
 
-	fftwf_free(in);
-	fftwf_free(out);
-	return 0;
+    fftwf_free(in);
+    fftwf_free(out);
+    return 0;
 }
